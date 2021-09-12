c773026786c682d0255071d1b7f09a3e0ee8095d
unknown <shubhamchau78@gmail.com>
2021-09-13 00:27:44 +0530

    raw code added in videocapturer

--- utils/mbl_sys_connector.py
+++ utils/mbl_sys_connector.py
@@ -1,11 +1,27 @@
 # this file will help to connect mobile cam with the system & provides the images / Videos
 # inputs takes the adress of the mobile or cctv for connection 
 
+import cv2
 
 class getVidFromCam:
     def __init__(self , couplingAddress ) -> None:
         self.couplingAdress = couplingAddress
-        pass 
+
+
+    def connect(self):
+        capture = cv2.VideoCapture(self.couplingAddress)
+        while True : 
+            # _ == is there video or not 
+            _ , frame = capture.read()
+            cv2.imshow('ivestream , frame')
+            if cv2.waitkey(1) == ord('q') : 
+                break 
+
+        capture.realease()
+        cv2.destroyAllwindows()
+
+
+
 
 
     
