6d38692ba140af3d2b25b7ba1d5b48071328d2d9
unknown <shubhamchau78@gmail.com>
2021-09-13 00:05:07 +0530

    utils updated

--- utils/mbl_sys_connector.py
+++ utils/mbl_sys_connector.py
@@ -1,6 +1,7 @@
 # this file will help to connect mobile cam with the system & provides the images / Videos
+# inputs takes the adress of the mobile or cctv for connection 
 
 
 class getVidFromCam:
-    def __init__(self) -> None:
+    def __init__(self , couplingAddress ) -> None:
         pass 
---
+++ utils/registration_utils/registrarionimageTaker.py
@@ -0,0 +1,6 @@
+# this file will help to take the images for registration purpose 
+# input it'll take the path of the directory where images want to store for the training 
+
+class imagetaker:
+    def __init__(sel , pathTostore) -> None:
+        pass
