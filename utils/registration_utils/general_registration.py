a8cac17ceeea8e7b76347953fd0a420d61b74fe9
unknown <shubhamchau78@gmail.com>
2021-09-13 22:23:54 +0530

    Registration utils updated

--- utils/registration_utils/general_registration.py
+++ utils/registration_utils/general_registration.py
@@ -1,5 +1,10 @@
-import os 
-
+# we can use database to store the images of authorised persons 
+        # thre will be two section in database 
+                #  1.Permanent : having permanent data which can't be deleted ( for history purpose & historical records )
+                #  2. Changable : In which new client data will be stored and client info will be deleted as client leaves the 
+                #                 orgamization & algo will be run on this data .
+                # 
+import os
 
 class register:
     def __init__(self , client_name , client_id ):
